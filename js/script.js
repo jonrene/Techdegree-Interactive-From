762c607c82cacbdf8f66e139a1af284fa4821729
jonrene <jrjonathanrene@gmail.com>
2021-10-31 19:18:24 -0400

    Other Job Role
    Automatically makes "other job role" text field hidden. If user selects "other" for his or her job, "other job role" text field becomes visible.

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Jonathan Rene
+// Techdegree - Project 3
+// Javascript Code
+// 10/31/2021
+
+// This automatically focuses on the name field when the page first loads. 
+document.getElementById("name").focus();
+
+// This automatically makes the "other job role" text field hidden when the page first loads. 
+document.getElementById("other-job-role").style.display = 'none';
+
+// This detects if the user has selected "other" for his or her job role.
+// If so, the "other job role" text field becomes visible
+document.getElementById("title").addEventListener('change', (e) => {
+    if (e.target.value === "other") {
+        document.getElementById("other-job-role").style.display = 'block';
+    } else{
+        document.getElementById("other-job-role").style.display = 'none';
+    }
+})
+
