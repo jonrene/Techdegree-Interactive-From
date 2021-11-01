78cebc45d316d91baabe56da3458130e27fb251c
jonrene <jrjonathanrene@gmail.com>
2021-10-31 20:43:49 -0400

    T-shirt Color Options for Design
    This enables the t-shirt color selection once user selects a design. Also determines which colors to show based on design selection.

--- js/script.js
+++ js/script.js
@@ -21,5 +21,40 @@ document.getElementById("title").addEventListener('change', (e) => {
 
 // This automatically disables the t-shirt color selection element
 // when the page first loads.
-document.getElementById("color").setAttribute('disabled', true);
+document.getElementById("color").disabled = true;
 
+// This detects for change in the design selection method
+let design = document.getElementById("design");
+design.addEventListener('change', (e) => {
+    // Enables t-shirt color selection method
+    document.getElementById("color").disabled = false;
+
+    // Sets color choice back to default (I created this default)
+    document.getElementById('color').value = "Select T-shirt Color";
+
+    // This determines which color options to be shown for t-shirt color based on design user selects. 
+    // This shows color options if user selects I "heart" JS Puns
+    if (design.value === "js puns"){
+        let colors = document.getElementById('color').children;
+        for(let i=0; i < colors.length; i++){
+            if (colors[i].value === "cornflowerblue" || colors[i].value === "darkslategrey" || colors[i].value === "gold"){
+                colors[i].hidden = false;
+            }
+            else{
+                colors[i].hidden = true;
+            }
+        }
+    // This shows color options if user selects I "heart" JS. 
+    } else{
+        let colors = document.getElementById('color').children;
+        for(let i=0; i < colors.length; i++){
+            if (colors[i].value === "tomato" || colors[i].value === "steelblue" || colors[i].value === "dimgrey"){
+                colors[i].hidden = false;
+            }
+            else{
+                colors[i].hidden = true;
+            }
+        }
+
+    }
+})
